6815cf2d19f2ed657573676af0d6b134d4ff3831
Peter Bailey <i@peterbailey.eu>
2016-05-19 19:27:49 +0100

    Dir rename

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@ require(['config'], function () {
     require(
         [
             'backbone',
-            'src/View/MainView'
+            'src/view/MainView'
         ],
         function (Backbone, MainView) {
            
