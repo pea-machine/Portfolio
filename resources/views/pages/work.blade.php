73787286514320f900e9d909827614869d98c297
Peter Bailey <prodigy.zero@gmail.com>
2016-06-01 13:46:06 +0000

    Lazy load video test

--- resources/views/pages/work.blade.php
+++ resources/views/pages/work.blade.php
@@ -43,7 +43,7 @@
 		<div class="video-wrapper pre-lay">
 			<?php /* <iframe src="https://player.vimeo.com/video/143464387?color=ffffff&title=0&byline=0&portrait=0" width="1000" height="562" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> */ ?>
 			
-			<div class"iframeLazyLoad" data-url="https://player.vimeo.com/video/143464387?color=ffffff&title=0&byline=0&portrait=0" data-classes=""></div>
+			<div class="iframeLazyLoad" data-url="https://player.vimeo.com/video/143464387?color=ffffff&title=0&byline=0&portrait=0" data-classes=""></div>
 		</div>
 	</div>
 	<div class="col perc50 smallest-to-100 smaller-to-100 small-to-100 medium-to-100">
