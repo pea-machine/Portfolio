ec8d48f7bd703ce370b82fc63112b5055260665a
Peter Bailey <prodigy.zero@gmail.com>
2016-06-08 16:23:31 +0000

    Bakcground sprite

--- public/js/src/view/DesktopView.js
+++ public/js/src/view/DesktopView.js
@@ -307,7 +307,7 @@ define(
                     { scale: 0.9, ease: Back.easeOut }, 
                     0.5);
                 var tl = new TimelineMax({ repeat: 300 });
-                tl.timeScale(5).
+                tl.timeScale(15).
                 to($('.logo #background image'), 0, { attr:{ y: '-300' } }, 0).
                 to($('.logo #background image'), 0, { attr:{ y: '-600' } }, 1).
                 to($('.logo #background image'), 0, { attr:{ y: '-900' } }, 2).
