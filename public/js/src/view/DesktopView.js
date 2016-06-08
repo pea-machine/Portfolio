748e5b27968356ab38070521c98311235c429062
Peter Bailey <prodigy.zero@gmail.com>
2016-06-08 16:17:07 +0000

    Bakcground sprite

--- public/js/src/view/DesktopView.js
+++ public/js/src/view/DesktopView.js
@@ -307,12 +307,12 @@ define(
                     { scale: 0.9, ease: Back.easeOut }, 
                     0.5);
                 var tl = new TimelineMax({ repeat: 300 });
-                tl.timeScale(5).
-                to($('.logo #background image'), 0, { attr:{ y: -300 } }, 0).
-                to($('.logo #background image'), 0, { attr:{ y: -600 } }, 0).
-                to($('.logo #background image'), 0, { attr:{ y: -900 } }, 0).
-                to($('.logo #background image'), 0, { attr:{ y: -1200 } }, 0).
-                to($('.logo #background image'), 0, { attr:{ y: -1500 } }, 0);
+                tl.timeScale(15).
+                to($('.logo #background image'), 0, { attr:{ y: '-300' } }, 0).
+                to($('.logo #background image'), 0, { attr:{ y: '-600' } }, 0).
+                to($('.logo #background image'), 0, { attr:{ y: '-900' } }, 0).
+                to($('.logo #background image'), 0, { attr:{ y: '-1200' } }, 0).
+                to($('.logo #background image'), 0, { attr:{ y: '-1500' } }, 0);
                 /*this.pageEvents.on('pagePopulated', function(msg) {
                     tl.stop();
                     $('.logo #background image').attr('y', '0');
