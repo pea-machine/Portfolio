61a038760bdccd605a4677749b2cfb02a9f5fb9d
Peter Bailey <prodigy.zero@gmail.com>
2016-07-07 14:45:25 +0000

    About page update

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -8,16 +8,14 @@
 		<div class="col perc75 smallest-to-100 smaller-to-100 small-to-100">
 			<div class="block-text">
 				<p>
-					Like most London creatives I grew up in one of the home counties; Kent. I studied electronics because "there's no jobs in web design". I later landed my first job in web design and unglamorously made ecommerce sites. But I was soon packing my bags and heading eastward to begin a new life in Old Street.
+					Full-stack nomad working with a range of technologies while floating around Laravel, Wordpress and Backbone. I've experience building for brands such as Sky, ASOS, Vue Cinemas, Diageo, EE, Macmillan and thrive on projects with high polish.
 				</p>
 				<p>
-					Tucked away in the back streets our small studio was busily overhauling the Witch? website. It allowed me to experiment with some early responsive-design principles, and I developed a Javascript WYSIWYG editor to output articles that adapted to the screen size.
+					Founded the online magazine <a href="http://techmog.com" target="_blank">Techmog.com</a> in 2011 and now manage a team of bloggers to cover London's product design community.
 				</p>
 				<p>
-					I then joined the then marketing agency Independents United where I was responsible for developing a range of digital campaigns for brands such as ASOS, Sky and Diageo. I also developed the internal venture Dare to Donate; a fundraising platform which has since raised thousands for charity. Over time the company changed and so did my role. It gave me the opportunity to learn about producing and I managed social for Diageo World Class. My day-to-day would include content creation and communicating it to the 30 global markets.
-				</p>
-				<p>
-					Concurrent to this I founded the hardware blog Techmog.com which covers product design and the maker movement in London. Today we are a team of writers who do an amazing job at showing what London is making.
+					Created Signature Wizard, a Chrome Extension that inserts dynamic HTML into Gmail emails.
+					<img src="/public/img/signature-wizard.jpg">
 				</p>
 			</div>
 		</div>
