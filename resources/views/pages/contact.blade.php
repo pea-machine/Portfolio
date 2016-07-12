52d3443919022b7507dc28143d882e5375b1a903
Peter Bailey <prodigy.zero@gmail.com>
2016-07-12 13:29:23 +0000

    Spaces instead of tabs

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -6,19 +6,19 @@
 				<a href="mailto:hi@peabay.xyz">hi@peabay.xyz</a></p>
 				<h3>Next Available</h3>
 				<?php
-					$nextAvailable = App\Models\Settings::where('name', 'next_available')->first();
-					echo date('D jS F', $nextAvailable->value);
+                    $nextAvailable = App\Models\Settings::where('name', 'next_available')->first();
+                    echo date('D jS F', $nextAvailable->value);
                 ?>
-			</div>
-		</div>
-		<div class="col perc50 smallest-to-100 smaller-to-100 small-to-100 medium-to-100">
-			<div class="block-text">
-				<h3>Follow</h3>
-				<ul>
-					<li>Twitter</li>
-					<li>Instagram</li>
-				</ul>
-			</div>
-		</div>
-	</div>
+            </div>
+        </div>
+        <div class="col perc50 smallest-to-100 smaller-to-100 small-to-100 medium-to-100">
+            <div class="block-text">
+                <h3>Follow</h3>
+                <ul>
+                    <li>Twitter</li>
+                    <li>Instagram</li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </div>
