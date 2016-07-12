c082e1e33669a1d6e6f414f807a1e6db6011896b
Peter Bailey <prodigy.zero@gmail.com>
2016-07-12 14:55:26 +0000

    Next available styling

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,24 +1,13 @@
 <div class="contact">
-	<div class="grid gut20">
-		<div class="col perc50 smallest-to-100 smaller-to-100 small-to-100 medium-to-100">
-			<div class="block-text">
-				<h2>Contact</h2>
-				<p><a href="mailto:hi@peabay.xyz">hi@peabay.xyz</a></p>
-				<h2>Next Available</h2>
-				<p><?php
-                    $nextAvailable = App\Models\Settings::where('name', 'next_available')->first();
-                    echo date('D jS F', $nextAvailable->value);
-                ?></p>
-            </div>
-        </div>
-        <div class="col perc50 smallest-to-100 smaller-to-100 small-to-100 medium-to-100">
-            <div class="block-text">
-                <h2>Follow</h2>
-                <ul>
-                    <li>Twitter</li>
-                    <li>Instagram</li>
-                </ul>
-            </div>
-        </div>
-    </div>
+	<div class="container">
+		<div class="next-available">
+			<?php
+				$nextAvailable = App\Models\Settings::where('name', 'next_available')->first();
+	            echo date('D jS F', $nextAvailable->value);
+	        ?>
+	        <div class="arrow"></div>
+	        <div class="label">Next Available</div>
+		</div>
+		<div class="email">hi@peabay.xyz</div>
+	</div
 </div>
