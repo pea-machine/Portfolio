7a1680ad207d5a252c55b3e729ed0b19fa58f9e4
Peter Bailey <prodigy.zero@gmail.com>
2016-07-12 15:49:50 +0000

    About page copy update

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,21 +1,15 @@
 <div class="page-about">
 	<div class="grid">
-		<div class="col perc25 smallest-to-100 smaller-to-100 small-to-100">
+		<div class="col perc100">
 			<div class="block-image">
 				<img src="/public/img/me.png" class="display-picture">
 			</div>
-		</div>
-		<div class="col perc75 smallest-to-100 smaller-to-100 small-to-100">
 			<div class="block-text">
 				<p>
-					Full-stack nomad working with a range of technologies while floating around Laravel, Wordpress and Backbone. I've experience building for brands such as Sky, ASOS, Vue Cinemas, Diageo, EE, Macmillan and thrive on projects with high polish.
+					Full-stack nomad working with a range of technologies while floating around Laravel, Wordpress and Backbone. I have experience building for brands such as Sky, ASOS, Vue Cinemas, Diageo, EE, Macmillan and thrive on projects with high polish.
 				</p>
 				<p>
-					Founded the online magazine <a href="http://techmog.com" target="_blank">Techmog.com</a> in 2011 and now manage a team of bloggers to cover London's product design community.
-				</p>
-				<h2>Signature Wizard</h2>
-				<p>
-					Chrome extension to insert dynamic HTML signatures into Gmail emails via urls. <a href="" class="button inline">View</a><a href="" class="button inline">Source</a>
+					I have a passion for making (hardware as well as software) and founded the product design blog Techmog.com in 2011. I'm now joined by a team of writers who help cover London's maker scene. 
 				</p>
 			</div>
 		</div>
