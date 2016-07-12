c293bb63dcf0ca4b39414a9e5237efc06bd0f344
Peter Bailey <prodigy.zero@gmail.com>
2016-07-12 12:42:15 +0000

    Database namespace

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Models\Settings;
+use DB;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -48,6 +49,6 @@ class Kernel extends ConsoleKernel
             $settings
                 ->where('name', 'next_available')
                 ->update(['value' => $next_available]);
-        })->everyFiveMinutes();
+        })->everyMinute();
     }
 }
