9a360dd7901f74131246da8bd70fe51bdad2eb91
Peter Bailey <prodigy.zero@gmail.com>
2016-07-12 14:31:37 +0000

    Contact page styles

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -2,18 +2,18 @@
 	<div class="grid gut20">
 		<div class="col perc50 smallest-to-100 smaller-to-100 small-to-100 medium-to-100">
 			<div class="block-text">
-				<h3>Contact</h3>
-				<a href="mailto:hi@peabay.xyz">hi@peabay.xyz</a></p>
-				<h3>Next Available</h3>
-				<?php
+				<h2>Contact</h2>
+				<p><a href="mailto:hi@peabay.xyz">hi@peabay.xyz</a></p>
+				<h2>Next Available</h2>
+				<p><?php
                     $nextAvailable = App\Models\Settings::where('name', 'next_available')->first();
                     echo date('D jS F', $nextAvailable->value);
-                ?>
+                ?></p>
             </div>
         </div>
         <div class="col perc50 smallest-to-100 smaller-to-100 small-to-100 medium-to-100">
             <div class="block-text">
-                <h3>Follow</h3>
+                <h2>Follow</h2>
                 <ul>
                     <li>Twitter</li>
                     <li>Instagram</li>
