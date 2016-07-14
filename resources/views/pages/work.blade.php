6677f3f96a9da267c77447bb376fa859d5e8a5ee
Peter Bailey <prodigy.zero@gmail.com>
2016-07-14 15:21:43 +0000

    Video posters

--- resources/views/pages/work.blade.php
+++ resources/views/pages/work.blade.php
@@ -11,7 +11,7 @@
             </div>
         </div>
         
-        <video width="100%" height="auto" class="videoLazyLoad playing">
+        <video width="100%" height="auto" class="videoLazyLoad playing" poster="/public/img/video/daretodonate_10s_placeholder.jpg">
             <source src="/public/img/video/daretodonate_10s_compressed.webm" type="video/webm">
             <source src="/public/img/video/daretodonate_10s_compressed.mp4" type="video/mp4">
             <img src="/public/img/video/daretodonate_10s_placeholder.jpg">
@@ -26,7 +26,7 @@
             </div>
         </div>
         
-        <video width="100%" height="auto" class="videoLazyLoad paused">
+        <video width="100%" height="auto" class="videoLazyLoad paused" poster="/public/img/video/techmog_10s_placeholder.jpg">
             <source src="/public/img/video/techmog_10s_compressed.webm" type="video/webm">
             <source src="/public/img/video/techmog_10s_compressed.mp4" type="video/mp4">
             <img src="/public/img/video/techmog_10s_placeholder.jpg">
@@ -43,7 +43,7 @@
             </div>
         </div>
         
-        <video width="100%" height="auto" class="videoLazyLoad paused">
+        <video width="100%" height="auto" class="videoLazyLoad paused" poster="/public/img/video/adorer_10s_placeholder.jpg">
             <source src="/public/img/video/Adorer_10s_compressed.webm" type="video/webm">
             <source src="/public/img/video/Adorer_10s_compressed.mp4" type="video/mp4">
             <img src="/public/img/video/adorer_10s_placeholder.jpg">
