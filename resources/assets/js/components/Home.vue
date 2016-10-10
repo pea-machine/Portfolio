44689f6962dbb35f759a4a4452babd2b6c6accbb
Peter Bailey <i@peterbailey.eu>
2016-10-10 18:03:19 +0100

    Added videos

--- resources/assets/js/components/Home.vue
+++ resources/assets/js/components/Home.vue
@@ -1,6 +1,21 @@
 <template>
     <div>
         <loader></loader>
+        <video width="100%" height="auto" class="videoLazyLoad playing" poster="/public/img/video/daretodonate_10s_placeholder.jpg">
+            <source src="/public/img/video/daretodonate_10s_compressed.webm" type="video/webm">
+            <source src="/public/img/video/daretodonate_10s_compressed.mp4" type="video/mp4">
+            <img src="/public/img/video/daretodonate_10s_placeholder.jpg">
+        </video>
+        <video width="100%" height="auto" class="videoLazyLoad paused" poster="/public/img/video/techmog_10s_placeholder.jpg">
+            <source src="/public/img/video/techmog_10s_compressed.webm" type="video/webm">
+            <source src="/public/img/video/techmog_10s_compressed.mp4" type="video/mp4">
+            <img src="/public/img/video/techmog_10s_placeholder.jpg">
+        </video>
+        <video width="100%" height="auto" class="videoLazyLoad paused" poster="/public/img/video/adorer_10s_placeholder.jpg">
+            <source src="/public/img/video/Adorer_10s_compressed.webm" type="video/webm">
+            <source src="/public/img/video/Adorer_10s_compressed.mp4" type="video/mp4">
+            <img src="/public/img/video/adorer_10s_placeholder.jpg">
+        </video>
     </div>
 </template>
 
