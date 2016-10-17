5017abd011089927c0a30e5ab17e338fa3f166c3
Peter Bailey <i@peterbailey.eu>
2016-10-17 18:26:36 +0100

    Smome stuff

---
+++ resources/assets/js/components/Container.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <loader></loader>
+        <burger></burger>
+        <transition name="fade"><router-view></router-view></transition>
+    </div>
+</template>
+
+<style lang="sass" scoped>
+
+</style>
+    
+<script>
+    import Loader from './Loader.vue';
+    import Burger from './Burger.vue';
+    import store from '../vuex/store.js';
+    export default {
+        components: {
+            'loader': Loader,
+            'burger': Burger
+        },
+        mounted() {
+            store.commit('updatePageLoadingStatus', true);
+        }
+    }
+</script>
