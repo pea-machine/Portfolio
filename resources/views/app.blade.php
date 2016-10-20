bd96b623a50f3c08f51afcd338f78ec5da99a725
Peter Bailey <i@peterbailey.eu>
2016-10-20 18:09:31 +0100

    OG tags, favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link href="/public/css/app.css" media="all" rel="stylesheet" />
+        <link rel="icon" type="image/png" href="{{ URL::asset('public/img/palmemoji.png') }}">
+        <meta property="og:url" content="http://peabay.xyz"/>
+        <meta property="og:title" content="Peter Bailey / Wordpress Developer London"/>
+        <meta property="og:site_name" content="Peter Bailey"/>
+        <meta property="og:description" content="Portfolio of Peter Bailey"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:image" content="{{ URL::asset('public/img/display-picture.jpg') }}"/>
+        <meta property="og:locale" content="en_gb"/>
         <title>Peter Bailey / Wordpress Developer London</title>
     </head>
     <body>
